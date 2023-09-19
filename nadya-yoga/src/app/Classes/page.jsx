2eb776aed59bf68d9571f18a9ec878024d8c18f8
Joan <joan.baare@gmail.com>
2023-09-19 20:01:38 +1000

    Add classes and timetable pages

--- nadya-yoga/src/app/Classes/page.jsx
+++ nadya-yoga/src/app/Classes/page.jsx
@@ -1,11 +1,83 @@
+'use client'
 import * as React from 'react';
+import { useRouter } from 'next/navigation'
+import Grid from '@mui/material/Grid';
+import CustomCard from '@/components/CustomCard';
+
+
+
 
 export default function Classes() {
-
-    return (
+  const router = useRouter()
+  return (
+    
         <div>
-            <h2>Classes Page</h2>
-            
-        </div>
-    );
+          
+        <h2>Classes</h2>
+        <button onClick={() => router.push('/Timetable')}>
+      Timetable 
+    </button>
+   
+          <Grid container spacing={2} my={2}>    
+              <Grid item xs={12} md={6}> 
+                  <CustomCard 
+                    title="All About Balance" 
+                    subheader="14/October/2023"
+                    image="/balance.jpeg"
+                    alt="small rock balacing" >                    
+                    Whether you want to practice yoga because of a vestibular disorder
+                      or to improve your balance, you're on the right track.
+                       Yoga helps with balance, focus, movement, and coordination.
+                        Rather than focusing on poses and being still, balance comes from movement, 
+                        mastering transitions, and developing your strength                     
+                  </CustomCard> 
+              </Grid>
+                <Grid item xs={12} md={6}> 
+                <CustomCard 
+                    title="All About Breath" 
+                    subheader="18/November/2023"
+                    image="/yoga-card-image.jpeg"
+                    alt="woman doing yoga" >                    
+                    Whether you want to practice yoga because of a vestibular disorder
+                      or to improve your balance, you're on the right track.
+                       Yoga helps with balance, focus, movement, and coordination.
+                        Rather than focusing on poses and being still, balance comes from movement, 
+                        mastering transitions, and developing your strength                     
+                  </CustomCard> 
+                </Grid>             
+          </Grid>
+       
+   
+        <Grid container spacing={2} my={2}>    
+            <Grid item xs={12} md={6}> 
+                <CustomCard 
+                  title="All About Balance" 
+                  subheader="14/October/2023"
+                  image="/balance.jpeg"
+                  alt="small rock balacing" >                    
+                  Whether you want to practice yoga because of a vestibular disorder
+                    or to improve your balance, you're on the right track.
+                     Yoga helps with balance, focus, movement, and coordination.
+                      Rather than focusing on poses and being still, balance comes from movement, 
+                      mastering transitions, and developing your strength                     
+                </CustomCard> 
+            </Grid>
+              <Grid item xs={12} md={6}> 
+              <CustomCard 
+                  title="All About Breath" 
+                  subheader="18/November/2023"
+                  image="/yoga-card-image.jpeg"
+                  alt="woman doing yoga" >                    
+                  Whether you want to practice yoga because of a vestibular disorder
+                    or to improve your balance, you're on the right track.
+                     Yoga helps with balance, focus, movement, and coordination.
+                      Rather than focusing on poses and being still, balance comes from movement, 
+                      mastering transitions, and developing your strength                     
+                </CustomCard> 
+              </Grid>             
+        </Grid>
+      </div> 
+      
+   
+  )
 }
--- nadya-yoga/src/app/Timetable/page.jsx
+++ nadya-yoga/src/app/Timetable/page.jsx
@@ -1,11 +1,66 @@
 import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, Tuesday,Thursday, Friday, Saturday, Sunday) {
+  return { name, Tuesday, Thursday, Friday, Saturday, Sunday };
+}
+
+const rows = [
+  createData('9 AM', 'Vinyasa(Flow)', 'Prenatal', 'Vinyasa', 'Vinyasa', ' Community Class' ),
+  createData('4:30 PM', 'Vinyasa(Flow)', 'Prenatal', 'Vinyasa', 'Vinyasa', ' Community Class'),
+ 
+];
 
 export default function Timetable() {
-
-    return (
-        <div>
-            <h2>Timetable Page</h2>
+  return (
+    <div>
+        <h2>Weekly Schedule</h2>
+        <p>No need to book in advance, just show up and pay at the front. Our lovely receptionist will welcome you</p>
+        <p>Please arrive 15 mins before class so as to get settled in</p>
+        <p>All our classes are 1 hour</p>
+    
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Time</TableCell>
             
-        </div>
-    );
+            <TableCell align="right">Tuesday</TableCell>
+           
+            <TableCell align="right">Thursday</TableCell>
+            <TableCell align="right">Friday</TableCell>
+            <TableCell align="right">Saturday</TableCell>
+            <TableCell align="right">Sunday</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              
+              <TableCell align="right">{row.Tuesday}</TableCell>
+              
+              <TableCell align="right">{row.Thursday}</TableCell>
+              <TableCell align="right">{row.Friday}</TableCell>
+              <TableCell align="right">{row.Saturday}</TableCell>
+              <TableCell align="right">{row.Sunday}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    
+    </div>
+  );
 }
